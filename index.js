070cb0db045d8b0f290d88c708110a47239bb16d
alexanderpeal <44208038+alexanderpeal@users.noreply.github.com>
2024-01-27 13:35:17 -0500

    can add tasks now, fixed files

--- index.js
+++ index.js
@@ -3,28 +3,32 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const taskRoutes = require('./routes/taskRoutes'); // import task routes
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('public')); // serve static files
 
 // connect to MongoDB
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri);
+mongoose.connect(uri, {useNewurlParser: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database successfully connected");
 });
 
+// Use routes
+app.use('/tasks', taskRoutes);
 
+// Serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
 
 // run server
 app.listen(5000, () => {
     console.log("Server running on port 5000");
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
 });
