4fadf2366997c8ee00a24f1f3cecb3d6c0824a35
alexanderpeal <44208038+alexanderpeal@users.noreply.github.com>
2024-01-29 21:15:45 -0500

    added validation to create

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const Task = require('../models/Task');
+const {body, validationResult} = require('express-validator');
 
-router.get('/', async (req, res) => {
-    try {
-        // Lean to fetch JSON
-        const tasks = await Task.find().lean();
-        res.render('layouts/tasks', {tasks: tasks});
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-});
-
-router.post('/add', async(req, res) => {
+// CREATE a new item
+router.post('/add', [
+    body('itemName').notEmpty().withMessage(
+        'itemName is a required field').isString().withMessage(
+        'itemName must be a string'
+    ),
+    body('description').optional().isString(),
+    body('deadline').optional().isISO8601().withMessage(
+        'deadline must be a valid date'
+    ),
+    body('complete').optional().isBoolean().withMessage(
+        'complete must be a boolean'),
+], async(req, res) => {
     const newTask = new Task({
         itemName: req.body.itemName,
         description: req.body.description,
@@ -26,7 +29,19 @@ router.post('/add', async(req, res) => {
     } catch (err) {
         res.status(400).json({message: err.message});
     }
-})
+});
+
+// READ all items
+router.get('/', async (req, res) => {
+    try {
+        // Lean fetches JSON. This way we can render using handlebars
+        const tasks = await Task.find().lean();
+        res.render('layouts/tasks', {tasks: tasks});
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+// UPDATE an item
 
 module.exports = router;
-
