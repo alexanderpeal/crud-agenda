5f32bdddc3e7e74cea92fcf186e2a00ffd23a77e
Alexander Peal <aapeal04@gmail.com>
2024-03-02 23:57:03 -0500

    fixing chai tests

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,18 +1,19 @@
 /**
- * index.js
  * Entry point for the application.
- * 2024-02-27
+ * 
+ * @requires express
+ * @since 2024-03-01
+ * @author Alexander Peal
  */
+
+// API versioning
+require('dotenv').config({path: `./.env`});
+const apiVersion = process.env.API_VERSION || 'v1';
 
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
-// API versioning
-require('dotenv').config({path: `./.env`});
-
-const apiVersion = process.env.API_VERSION || 'v1';
 const taskRouter = require(`./src/${apiVersion}/routes/task-router`);
 
 // Configure express
@@ -35,19 +36,6 @@ app.listen(3001, () => {
     console.log(`Server running on port 3001, API ${apiVersion}`);
 });
 
-// Handlebars view engine setup
-// const path = require('path');
-// const {engine} = require('express-handlebars');
-// app.set('view engine', 'handlebars');
-// const layoutsDirPath = path.join(__dirname, `/${apiVersion}/views/layouts`)
-// app.engine('handlebars', engine({
-//     defaultLayout: 'tasks',
-//     layoutsDir: path.join(__dirname, `/${apiVersion}/views/layouts`)
-// }));
-// app.set('views', layoutsDirPath);
+// Export for testing purposes
+module.exports = app;
 
-// API routes
-// Serve index.html for the root route
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, `./${apiVersion}/views/index.html`));
-// });
